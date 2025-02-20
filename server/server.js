bfcf1d2403b1db9103fda99939739b99e0825a71
dhushyandan <dhushyandan302002@gmail.com>
2025-02-20 11:15:42 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+const routes = require("./routes/students");
+
+const app = express();
+const port = 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+// API routes
+app.use('/', routes);
+
+//mognodb connection
+mongoose.connect('mongodb://localhost:27017/crudDB', {
+  
+}).then(() => {
+    console.log('MongoDB connected');
+}).catch(err => {
+    console.error('MongoDB connection error', err);
+    process.exit(1); // Exit process with failure
+});
+
+
+
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+
+
+app.get('*', cors(), (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://10.10.192.188:${port}`);
+});
