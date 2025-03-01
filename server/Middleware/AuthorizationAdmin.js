d0de18ccc057000b6d7439b7c19f0eb4a504c529
DHUSHYANDAN V <135136525+DHUSHYANDAN@users.noreply.github.com>
2025-03-01 09:49:20 +0530

    Delete server directory

--- server/Middleware/AuthorizationAdmin.js
+++
@@ -1,9 +0,0 @@
-// Middleware to check if the user is an Admin or Manager
-const authorizeAdminOrManager = (req, res, next) => {
-    if (req.user.role === 'Admin' ) {
-        next(); // Proceed to the route handler
-    } else {
-        return res.status(403).json({ message: 'Access denied. Only Admins can perform this action.' });
-    }
-};
-module.exports =  authorizeAdminOrManager; 
